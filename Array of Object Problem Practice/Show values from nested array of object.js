50cdb75ffdd1d55311d1afc0a869fe4af6f1453e
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2021-08-16 23:19:14 +0600

    Update Show values from nested array of object.js

--- Array of Object Problem Practice/Show values from nested array of object.js
+++ Array of Object Problem Practice/Show values from nested array of object.js
@@ -1,1 +1,46 @@
+//[ 'a', 'b', 'c', 'd' ]
+//[ 102, 107 ]
+var arr = [
+  {
+    product: "a",
+    weight: 102,
+    description: {
+      label: {
+        name: "a",
+      },
+    },
+  },
+  {
+    product: "b",
+    weight: 105,
+    description: {
+      label: {
+        name: "b",
+      },
+    },
+  },
+  {
+    product: "c",
+    weight: 107,
+    description: {
+      data: {
+        name: "c",
+      },
+    },
+  },
+  {
+    product: "d",
+    weight: 109,
+    description: {
+      label: {
+        name: "d",
+      },
+    },
+  }
+];
+// const label = []
+const label = arr.map(item=>item.description).map(i=>i.label || i.data).map(j=>j.name)
+console.log(label);
 
+const weight = arr.filter(item=> item.product === "a" || item.product==="c").map(i=>i.weight);
+console.log(weight);
